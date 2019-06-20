6262c29c4b0b76c7db8495c847162879560e97ce
Ben Jenkins <ben.jenkins@icloud.com>
2019-06-20 10:59:34 -0500

    help desk time

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const layout = require('./views/layout.js');
 const Sequelize = require('sequelize');
-const { db } = require('./models/index.js');
+const models = require('./models/index.js');
 
 const app = express();
 
@@ -10,37 +10,26 @@ app.use(morgan('dev'));
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: false }));
 
-db.authenticate().
-then(() => {
-  console.log('connected to the database');
-})
-
-const Page = db.define('page', {
-  title: Sequelize.STRING,
-  slug: Sequelize.STRING,
-  content: Sequelize.TEXT,
-  status: Sequelize.BOOLEAN
-})
-
-const User = db.define('user', {
-  name: Sequelize.STRING,
-  email: Sequelize.STRING
-})
+// db.authenticate().then(() => {
+//   console.log('connected to the database');
+// });
 
 //remember to wrap
+const port = 1337;
+const init = async () => {
+  try {
+    await models.User.sync();
+    await models.Page.sync();
+  } catch (err) {
+    console.log(err);
+  }
+  app.listen(port, () => {
+    console.log(`App is listening on port: ${port}`);
+  });
+};
 
-async function testDB() {
-  await User.sync()
-  await Page.sync()
-}
-
-testDB()
+init();
 
 app.get('/', async (req, res, next) => {
   res.send(layout('a'));
-});
-
-const port = 1337;
-app.listen(port, () => {
-  console.log(`App is listening on port: ${port}`);
 });
