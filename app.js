f9c9d669189dc21d49429777d5e1e477ca2e546c
Tom <tomsinovich@gmail.com>
2019-06-20 10:40:35 -0500

    adds db define logic

--- app.js
+++ app.js
@@ -1,18 +1,40 @@
 const express = require('express');
 const morgan = require('morgan');
 const layout = require('./views/layout.js');
-const { db } = require('./models');
+const Sequelize = require('sequelize');
+const { db } = require('./models/index.js');
 
 const app = express();
+
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/public'));
+app.use(express.urlencoded({ extended: false }));
 
 db.authenticate().
 then(() => {
   console.log('connected to the database');
 })
 
-app.use(morgan('dev'));
-app.use(express.static(__dirname + '/public'));
-app.use(express.urlencoded({ extended: false }));
+const Page = db.define('page', {
+  title: Sequelize.STRING,
+  slug: Sequelize.STRING,
+  content: Sequelize.TEXT,
+  status: Sequelize.BOOLEAN
+})
+
+const User = db.define('user', {
+  name: Sequelize.STRING,
+  email: Sequelize.STRING
+})
+
+//remember to wrap
+
+async function testDB() {
+  await User.sync()
+  await Page.sync()
+}
+
+testDB()
 
 app.get('/', async (req, res, next) => {
   res.send(layout('a'));
