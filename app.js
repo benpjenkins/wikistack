ad398ef45475ed3a64657e6ac8dd83936288aeef
Tom <tomsinovich@gmail.com>
2019-06-20 10:20:16 -0500

    creates model directory and adds db initialization into app.js

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const morgan = require('morgan');
 const layout = require('./views/layout.js');
+const { db } = require('./models');
 
 const app = express();
+
+db.authenticate().
+then(() => {
+  console.log('connected to the database');
+})
 
 app.use(morgan('dev'));
 app.use(express.static(__dirname + '/public'));
